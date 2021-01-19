d897476bf1a1d1519aa92a273d0d51aa0b9eae6e
carlosvpn15 <enicma15@gmail.com>
2021-01-18 23:08:33 -0500

    up 2021

---
+++ script.js
@@ -0,0 +1,8 @@
+
+// $(window).scroll(function () {
+//     if ($(window).scrollTop() >= 150) {
+//     $('#navbar1').css('background','#eee');
+//     } else {
+//     $('#navbar1').css('background','transparent');
+//     }
+//     });
