99cd4a46d87a1febf223f1c65ca2d35421be572c
carlosvpn15 <enicma15@gmail.com>
2021-01-23 20:52:54 -0500

    message

--- script.js
+++ script.js
@@ -1,8 +1,37 @@
 
+//TO CHANGE THE STYLE OF THE NAVBAR ON SCROL
+
 // $(window).scroll(function () {
 //     if ($(window).scrollTop() >= 150) {
-//     $('#navbar1').css('background','#eee');
+//     $('#navbar1').css('display','none');
 //     } else {
-//     $('#navbar1').css('background','transparent');
+//     $('#navbar1').css('display','block');
 //     }
 //     });
+
+
+//TO HIDE OR SHOW THE NAVBAR ON MOBILE
+$(function() {
+    $(".toggle").on("click", function (){
+        if($(".item").hasClass("active")) {
+            $(".item").removeClass("active");
+        }
+        else {
+            $(".item").addClass("active");
+        }
+    })
+});
+
+
+//TO CHANGE MOBILE-NAVBAR-ICON ON CLICK
+$(function() {
+    $(".toggle").on("click", function (){
+        if($(".toggle span").hasClass("fa-bars")) {
+            $(".toggle span").toggleClass("fa-times");
+        }
+        else {
+            $(".toggle span").addClass("fa-bars");
+        }
+    })
+});
+
